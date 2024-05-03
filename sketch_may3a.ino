0f54d465b8a3683565aeac0596c8ad3dfcb0dc4e
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2024-05-03 22:54:29 +0530

    commit

---
+++ sketch_may3a.ino
@@ -0,0 +1,456 @@
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+#include "DHT.h"
+#define DHTPIN D1
+#define DHTTYPE DHT11
+#define RAIN_SENSOR_PIN D3
+
+#include <Firebase_ESP_Client.h>
+const int pulsePin = A0;              // Pulse Sensor purple wire connected to analog pin A0
+
+// Volatile Variables, used in the interrupt service routine!
+volatile int BPM;                  // int that holds raw Analog in 0. updated every 2mS
+volatile int Signal;               // holds the incoming raw data
+volatile int IBI = 600;            // int that holds the time interval between beats! Must be seeded! 
+volatile boolean Pulse = false;    // "True" when User's live heartbeat is detected. "False" when not a "live beat". 
+volatile boolean QS = false;       // becomes true when Arduino finds a beat.
+
+volatile int rate[10];             // array to hold last ten IBI values
+volatile unsigned long sampleCounter = 0;   // used to determine pulse timing
+volatile unsigned long lastBeatTime = 0;    // used to find IBI
+volatile int P = 512;              // used to find peak in pulse wave, seeded
+volatile int T = 512;              // used to find trough in pulse wave, seeded
+volatile int thresh = 525;         // used to find instant moment of heart beat, seeded
+volatile int amp = 100;            // used to hold amplitude of pulse waveform, seeded
+volatile boolean firstBeat = true; // used to seed rate array so we startup with reasonable BPM
+volatile boolean secondBeat = false; 
+int a;// used to seed rate array so we startup with reasonable BPM
+
+os_timer_t timer;
+DHT dht(DHTPIN, DHTTYPE);
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+#define API_KEY "AIzaSyALcg6utMHov-wQwTfgqzUTSetnbd9AKFM"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://major-757a9-default-rtdb.firebaseio.com/" 
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+//unsigned long sendDataPrevMillis = 0;
+//int count = 0;
+bool signupOK = false;
+//Variables
+int i = 0;
+int statusCode;
+const char* ssid = "text";
+const char* passphrase = "text";
+String st;
+String content;
+
+
+//Function Decalration
+bool testWifi(void);
+void launchWeb(void);
+void setupAP(void);
+
+//Establishing Local server at port 80 whenever required
+ESP8266WebServer server(80);
+
+void setup()
+{
+pinMode(DHTPIN, INPUT);
+  pinMode(RAIN_SENSOR_PIN, INPUT);
+  dht.begin();
+  Serial.begin(9600); //Initialising if(DEBUG)Serial Monitor
+  Serial.println();
+  Serial.println("Disconnecting previously connected WiFi");
+  WiFi.disconnect();
+  EEPROM.begin(512); //Initialasing EEPROM
+  delay(10);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println();
+  Serial.println();
+  Serial.println("Startup");
+
+  //---------------------------------------- Read eeprom for ssid and pass
+  Serial.println("Reading EEPROM ssid");
+
+  String esid;
+  for (int i = 0; i < 32; ++i)
+  {
+    esid += char(EEPROM.read(i));
+  }
+  Serial.println();
+  Serial.print("SSID: ");
+  Serial.println(esid);
+  Serial.println("Reading EEPROM pass");
+
+  String epass = "";
+  for (int i = 32; i < 96; ++i)
+  {
+    epass += char(EEPROM.read(i));
+  }
+  Serial.print("PASS: ");
+  Serial.println(epass);
+
+
+  WiFi.begin(esid.c_str(), epass.c_str());
+  if (testWifi())
+  {
+    Serial.println("Succesfully Connected!!!");
+     /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+   if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+   os_timer_setfn(&timer, timer_interrupt_handler, NULL);
+  os_timer_arm(&timer, 5, true); 
+    // return;
+  }
+  else
+  {
+    Serial.println("Turning the HotSpot On");
+    launchWeb();
+    setupAP();// Setup HotSpot
+  }
+
+  Serial.println();
+  Serial.println("Waiting.");
+  
+  while ((WiFi.status() != WL_CONNECTED))
+  {
+    Serial.print(".");
+    delay(100);
+    server.handleClient();
+  }
+ 
+}
+void loop() {
+ 
+    if (QS == true) // A Heartbeat Was Found
+  {     
+   a = SerialOutputWhenBeatHappens(); 
+    QS = false; // reset the Quantified Self flag for next time    
+  }
+  
+  delay(20);
+  if ((WiFi.status() == WL_CONNECTED))
+  {
+
+    for (int i = 0; i < 10; i++)
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(1000);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(1000);
+    }
+   
+  
+// float h = dht.readHumidity();
+int sensorValue = digitalRead(RAIN_SENSOR_PIN);
+  float t = dht.readTemperature();
+  float ft=(t*9/5)+32;
+  if (Firebase.ready() && signupOK ) {
+        if (Firebase.RTDB.setString(&fbdo, "users/123/heartRate", a)){
+       Serial.print("Heart: ");
+       Serial.println(a);
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON 1: " + fbdo.errorReason());
+    }
+    if (Firebase.RTDB.setString(&fbdo, "users/123/water", String(sensorValue))){
+       Serial.print("Water Detect: ");
+       Serial.println(sensorValue);
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    
+    // Write a Float number on the database path test/float
+    if (Firebase.RTDB.setString(&fbdo, "users/123/temperature", String(ft))){
+       Serial.print("Temperature: ");
+       Serial.println(ft);
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  }
+  Serial.println("______________________________");
+  }
+  else
+  {
+  }
+
+}
+int SerialOutputWhenBeatHappens()
+{    
+  
+  return BPM;
+}
+
+void sendDataToSerial(char symbol, int data )
+{
+  Serial.print(symbol);
+  Serial.println(data);                
+}
+
+void timer_interrupt_handler(void *arg)
+{  
+  Signal = analogRead(pulsePin);              // read the Pulse Sensor 
+  sampleCounter += 2;                         // keep track of the time in mS with this variable
+  int N = sampleCounter - lastBeatTime;       // monitor the time since the last beat to avoid noise
+                                              //  find the peak and trough of the pulse wave
+  if (Signal < thresh && N > (IBI/5)*3) // avoid dichrotic noise by waiting 3/5 of last IBI
+  {      
+    if (Signal < T) // T is the trough
+    {                        
+      T = Signal; // keep track of lowest point in pulse wave 
+    }
+  }
+
+  if (Signal > thresh && Signal > P)
+  {          // thresh condition helps avoid noise
+    P = Signal;                             // P is the peak
+  }                                        // keep track of highest point in pulse wave
+
+  //  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
+  // signal surges up in value every time there is a pulse
+  if (N > 250)
+  {                                   // avoid high frequency noise
+    if ( (Signal > thresh) && (Pulse == false) && (N > (IBI/5)*3) )
+    {        
+      Pulse = true;                               // set the Pulse flag when we think there is a pulse
+      IBI = sampleCounter - lastBeatTime;         // measure time between beats in mS
+      lastBeatTime = sampleCounter;               // keep track of time for next pulse
+  
+      if (secondBeat)
+      {                        // if this is the second beat, if secondBeat == TRUE
+        secondBeat = false;                  // clear secondBeat flag
+        for(int i=0; i<=9; i++) // seed the running total to get a realistic BPM at startup
+        {             
+          rate[i] = IBI;                      
+        }
+      }
+  
+      if (firstBeat) // if it's the first time we found a beat, if firstBeat == TRUE
+      {                         
+        firstBeat = false;                   // clear firstBeat flag
+        secondBeat = true;                   // set the second beat flag
+        return;                              // IBI value is unreliable so discard it
+      }   
+      // keep a running total of the last 10 IBI values
+      word runningTotal = 0;                  // clear the runningTotal variable    
+
+      for(int i=0; i<=8; i++)
+      {                // shift data in the rate array
+        rate[i] = rate[i+1];                  // and drop the oldest IBI value 
+        runningTotal += rate[i];              // add up the 9 oldest IBI values
+      }
+
+      rate[9] = IBI;                          // add the latest IBI to the rate array
+      runningTotal += rate[9];                // add the latest IBI to runningTotal
+      runningTotal /= 10;                     // average the last 10 IBI values 
+      BPM = 60000/runningTotal;               // how many beats can fit into a minute? that's BPM!
+      QS = true;                              // set Quantified Self flag 
+      // QS FLAG IS NOT CLEARED INSIDE THIS ISR
+    }                       
+  }
+
+  if (Signal < thresh && Pulse == true)
+  {   // when the values are going down, the beat is over
+    Pulse = false;                         // reset the Pulse flag so we can do it again
+    amp = P - T;                           // get amplitude of the pulse wave
+    thresh = amp/2 + T;                    // set thresh at 50% of the amplitude
+    P = thresh;                            // reset these for next time
+    T = thresh;
+  }
+
+  if (N > 2500)
+  {                           // if 2.5 seconds go by without a beat
+    thresh = 512;                          // set thresh default
+    P = 512;                               // set P default
+    T = 512;                               // set T default
+    lastBeatTime = sampleCounter;          // bring the lastBeatTime up to date        
+    firstBeat = true;                      // set these to avoid noise
+    secondBeat = false;                    // when we get the heartbeat back
+  }
+} 
+
+//----------------------------------------------- Fuctions used for WiFi credentials saving and connecting to it which you do not need to change 
+bool testWifi(void)
+{
+  int c = 0;
+  Serial.println("Waiting for Wifi to connect");
+  while ( c < 20 ) {
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      return true;
+    }
+    delay(500);
+    Serial.print("*");
+    c++;
+  }
+  Serial.println("");
+  Serial.println("Connect timed out, opening AP");
+  return false;
+}
+
+void launchWeb()
+{
+  Serial.println("");
+  if (WiFi.status() == WL_CONNECTED)
+    Serial.println("WiFi connected");
+  Serial.print("Local IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("SoftAP IP: ");
+  Serial.println(WiFi.softAPIP());
+  createWebServer();
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+}
+
+void setupAP(void)
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0)
+    Serial.println("no networks found");
+  else
+  {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+  st = "<ol>";
+  for (int i = 0; i < n; ++i)
+  {
+    // Print SSID and RSSI for each network found
+    st += "<li>";
+    st += WiFi.SSID(i);
+    st += " (";
+    st += WiFi.RSSI(i);
+
+    st += ")";
+    st += (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*";
+    st += "</li>";
+  }
+  st += "</ol>";
+  delay(100);
+  WiFi.softAP("Batch-4 Safe Paws", "");
+  Serial.println("softap");
+  launchWeb();
+  Serial.println("over");
+}
+
+void createWebServer()
+{
+ {
+    server.on("/", []() {
+
+      IPAddress ip = WiFi.softAPIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+      content = "<!DOCTYPE HTML>\r\n<html>Hello from ESP8266 at ";
+      content += "<form action=\"/scan\" method=\"POST\"><input type=\"submit\" value=\"scan\"></form>";
+      content += ipStr;
+      content += "<p>";
+      content += st;
+      content += "</p><form method='get' action='setting'><label>SSID: </label><input name='ssid' length=32><input name='pass' length=64><input type='submit'></form>";
+      content += "</html>";
+      server.send(200, "text/html", content);
+    });
+    server.on("/scan", []() {
+      //setupAP();
+      IPAddress ip = WiFi.softAPIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+
+      content = "<!DOCTYPE HTML>\r\n<html>go back";
+      server.send(200, "text/html", content);
+    });
+
+    server.on("/setting", []() {
+      String qsid = server.arg("ssid");
+      String qpass = server.arg("pass");
+      if (qsid.length() > 0 && qpass.length() > 0) {
+        Serial.println("clearing eeprom");
+        for (int i = 0; i < 96; ++i) {
+          EEPROM.write(i, 0);
+        }
+        Serial.println(qsid);
+        Serial.println("");
+        Serial.println(qpass);
+        Serial.println("");
+
+        Serial.println("writing eeprom ssid:");
+        for (int i = 0; i < qsid.length(); ++i)
+        {
+          EEPROM.write(i, qsid[i]);
+          Serial.print("Wrote: ");
+          Serial.println(qsid[i]);
+        }
+        Serial.println("writing eeprom pass:");
+        for (int i = 0; i < qpass.length(); ++i)
+        {
+          EEPROM.write(32 + i, qpass[i]);
+          Serial.print("Wrote: ");
+          Serial.println(qpass[i]);
+        }
+        EEPROM.commit();
+
+        content = "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}";
+        statusCode = 200;
+        ESP.reset();
+      } else {
+        content = "{\"Error\":\"404 not found\"}";
+        statusCode = 404;
+        Serial.println("Sending 404");
+      }
+      server.sendHeader("Access-Control-Allow-Origin", "*");
+      server.send(statusCode, "application/json", content);
+
+    });
+  } 
+}
